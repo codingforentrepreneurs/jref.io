056903c6382a4fca9d4b45310a3b242b44e945b9
CodingForEntrepreneurs <hello@teamcfe.com>
2023-07-11 16:23:58 -0600

    updated homepage

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
-      <h1>jref.io is coming soon.</h1>
+      <h1>jref.io is coming soon!</h1>
     </main>
   )
 }
