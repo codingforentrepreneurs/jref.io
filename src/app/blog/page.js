b8d25bb969cb25d8c89ceb97e5558ac490fad89a
CodingForEntrepreneurs <hello@teamcfe.com>
2023-07-12 11:21:43 -0600

    Added no-store cache to blog page lookup

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -14,6 +14,7 @@ async function getData() {
     const domain = getDomain()
     const endpoint = `${domain}/api/posts` // -> third party api request??
     const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
+    // const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
 
     if (!res.ok) {
         throw new Error("Failed to fetch data")
