82a559b9c514ef0567628b6b749a78c948794ec5
CodingForEntrepreneurs <hello@teamcfe.com>
2023-07-12 11:22:32 -0600

    Updated to no-store caching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -13,8 +13,8 @@ async function getData() {
     // 1 endpoint - API?
     const domain = getDomain()
     const endpoint = `${domain}/api/posts` // -> third party api request??
-    const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-    // const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
+    // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
+    const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
 
     if (!res.ok) {
         throw new Error("Failed to fetch data")
