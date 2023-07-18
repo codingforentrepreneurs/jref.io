40e94e5ee4788eecf4bfc996ef0999b46a834e2e
CodingForEntrepreneurs <hello@teamcfe.com>
2023-07-18 12:06:51 -0600

    Updated domain

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,5 +1,5 @@
 export default function getDomain() {
     const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http"
-    const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000"
+    const domain = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "localhost:3000"
     return `${protocol}://${domain}`
 }
